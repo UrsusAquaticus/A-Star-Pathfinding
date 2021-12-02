33081383578d3bd61b7acab4bff42c82b0762f91
Zach <64456163+UrsusAquaticus@users.noreply.github.com>
2021-12-02 19:33:26 +1300

    Add files via upload

---
+++ Scripts/Models/UnitInventory.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitInventory
+{
+    public List<InventoryObject> inventory;
+
+    public UnitInventory(){
+        inventory = new List<InventoryObject>();
+    }
+}
